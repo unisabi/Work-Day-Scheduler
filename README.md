bfb613f7cc5baa8e3027a84b4f0a744b75cf966b
Sabina Ceja-Siete <sabinasiete@gmail.com>
2022-10-31 22:16:47 -0700

    updated readme and js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Work-Day-Scheduler
+
+## Description
+Creating a schduele with an 8 hour interval by having different hours with past,current and future hours. By using html and javascript.
+
+## Resources
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/time
+- Lovely peers and tutors!
+
+## Contact Me
+sabinasiete@gmail.com
+
+## Screenshot
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
 //local storage. - event listener, is listening for the saving button, to storage the values of the text dexcription and time.
+addEventListener = "#save"
+localStorage.setItem("save", "my-value-01");
+
+var myValue = localStorage.getItem('save);
+
+console.log(myValue)
+
+var arrayData = [
+    {
+        "save": "value01"
+    },
+    {
+        "key02": "value02"
+    }
+]
+
+localStorage.setItem("myArray", JSON.stringify(arrayData));
+
+var myObj = localStorage.getItem('myArray');
+
+console.log(myObj);
+var parsedData = JSON.parse(myObj);
+console.log(parsedData)
+// trying to making the past,current, and future hours show the colors.
+for (let i = 0; i < timeBlocks.length; i++){
+    var timeBlocks = timeBlocks[i].children[0].innerHTML;
+    
+}
